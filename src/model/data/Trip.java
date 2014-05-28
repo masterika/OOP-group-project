af2716b5fa348648f9b57abb586772dd08edf6db
masterika <iguda11@freeuni.edu.ge>
2014-05-29 00:00:30 +0400

    pirveladi trip

--- src/model/data/Trip.java
+++ src/model/data/Trip.java
@@ -1,16 +1,48 @@
 package model.data;
-
-public class Trip {
+import java.util.ArrayList;
+import java.util.List;
+import model.data.users.Location;
+import model.data.users.Product;
+public class Trip implements Product{
 	private String id;
 	private String name;
-	public Trip(){
-		
+	private String type; // 2 men trip, 10 men group team or ect.
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	private int price;
+	private List<Location> Locations = new ArrayList<Location>();
+	public Trip(){		
 	}
 	public String getId(){
 		return id;
-	}
-	
+	}	
 	public String getName(){
 		return name;
+	}	
+	public void setId(String id) {
+		this.id = id;		
 	}
+	public List<Location> getLocations() {
+		return Locations;
+	}
+	public void setLocations(List<Location> locations) {
+		Locations = locations;
+	}
+	public void addLocation(Location location) {
+		Locations.add(location);
+	}
+	public void setName(String name) {
+		this.name = name;
+	}		
+	
 }
