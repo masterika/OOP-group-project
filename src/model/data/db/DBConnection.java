9c676f7b4b2d6a7d0d20b06c3ad6c0fdd5345651
masterika <iguda11@freeuni.edu.ge>
2014-06-21 18:36:47 +0400

    bazashi kodis gameorebis mospoba.giga searchi axal bazaze gamitane da wavshalot dzveli userstoragebi

--- src/model/data/db/DBConnection.java
+++ src/model/data/db/DBConnection.java
@@ -6,8 +6,8 @@ import java.sql.SQLException;
 
 public class DBConnection {
 	private static final String MYSQL_USERNAME = "root";
-	private static final String MYSQL_PASSWORD = "7676";
-	private static final String MYSQL_DATABASE_SERVER = "localhost:3306";
+	private static final String MYSQL_PASSWORD = "irakli500";
+	private static final String MYSQL_DATABASE_SERVER = "localhost";
 	private static final String MYSQL_DATABASE_NAME = "turista";
 	
 	private static Connection conn;
